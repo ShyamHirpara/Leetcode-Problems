5ccdd56f66fae7f47db18ef7ad5b8287ee2ab0ee
Shyam Hirpara <hiraparashyam666@gmail.com>
2026-07-25 22:29:31 +0530

    Time: 0 ms (100%), Space: 8.6 MB (89.66%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -3,15 +3,18 @@ public:
     int maxProduct(int n) {
         int max1 = 0;
         int max2 = 0;
-        while(n>0){
-            if(n%10>=max1){
-                max2 = max1;
-                max1 = n%10;
-            }else if(n%10<max1 && n%10 >max2){
-                max2 = n%10;
-            }
+        while (n > 0) {
+            max2 = n % 10 >= max1 ? max1 : (n % 10 > max2 ? n % 10 : max2);
+            max1 = n % 10 >= max1 ? n % 10 : max1;
+            // if (n % 10 >= max1) {
+            //     max2 = max1;
+            //     max1 = n % 10;
+            // }
+            // else if (n % 10 > max2) {
+            //     max2 = n % 10;
+            // }
             n = n / 10;
         }
-        return max1*max2;
+        return max1 * max2;
     }
 };
