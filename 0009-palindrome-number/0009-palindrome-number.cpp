35a6875895882ab49f18ef55857f645232a5d6e0
Shyam Hirpara <hiraparashyam666@gmail.com>
2026-07-27 22:53:49 +0530

    Time: 0 ms (100%), Space: 8.6 MB (65.67%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        vector<int> d;
-        if(x < 0)
+        long prev = x, rev = 0;
+        if (x < 0)
             return false;
-        while(x){
-            d.push_back(x%10);
+        while (x) {
+            rev = rev * 10 + x % 10;
             x /= 10;
         }
-        for(int i = 0; i < d.size()/2;i++){
-            if(d[i] != d[d.size()-1-i])
-                return false;
-        }
-
-        return true;
+        return prev == rev;
     }
 };
