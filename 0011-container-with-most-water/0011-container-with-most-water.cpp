6c9c691f871d637248282c864175bf6071555517
Shyam Hirpara <hiraparashyam666@gmail.com>
2026-07-27 21:55:47 +0530

    Time: 0 ms (100%), Space: 63 MB (11.28%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -6,7 +6,9 @@ public:
         while(l < r){
             // if(maxw < min(height[l],height[r])*(r-l))
             //     maxw = min(height[l],height[r])*(r-l);
-            maxw = max(maxw,min(height[l],height[r])*(r-l));
+            int w = r - l;
+            int h = height[l] < height[r] ? height[l] : height[r];
+            maxw = max(maxw,w*h);
             if(height[l] < height[r])
                 l++;
             else
